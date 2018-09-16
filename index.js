b8f72daa4b69175756508b86b49ed5ea405a3d0e
Mátyás Juhász <juhaszmtys@gmail.com>
2018-09-16 10:23:38 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+var player = require('play-sound')(opts = {})
+var Pusher = require('pusher-client');
+const say = require('say')
+
+var pusher = new Pusher(process.env.PUSHER_API_KEY, {
+  secret: process.env.PUSHER_SECRET,
+  appId: process.env.PUSHER_APP_ID,
+  cluster: process.env.PUSHER_CLUSTER,
+  encrypted: true
+});
+
+var my_channel = pusher.subscribe('palacsinta-channel');
+my_channel.bind('palacsinta-happened',
+  function(data) {
+    var random = 'sounds/' + (Math.floor(Math.random() * 3) + 1) + '.mp3';
+    var process = player.play(random, { detached: true }, function(err){
+      if (err) throw err;
+      if (data && data.name) say.speak('Good job ' + data.name + '!');
+    });
+    
+    process.unref();
+  }
+);
+
