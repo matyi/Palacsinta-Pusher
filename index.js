0948e93f193e34d7ec6d0df8e7964ea48207d58c
Mátyás Juhász <juhaszmtys@gmail.com>
2018-09-18 09:05:30 +0200

    make the code slightly prettier 💅

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-require('dotenv').config();
-var player = require('play-sound')(opts = {})
-var Pusher = require('pusher-client');
-const say = require('say')
+require("dotenv").config();
+const player = require("play-sound")((opts = {}));
+const Pusher = require("pusher-client");
+const say = require("say");
 
-var pusher = new Pusher(process.env.PUSHER_API_KEY, {
+const pusher = new Pusher(process.env.PUSHER_API_KEY, {
   cluster: process.env.PUSHER_CLUSTER,
   encrypted: true
 });
 
-var my_channel = pusher.subscribe('palacsinta-channel');
-my_channel.bind('palacsinta-happened',
-  function(data) {
-    var random = 'sounds/' + (Math.floor(Math.random() * 4) + 1) + '.mp3';
-    var process = player.play(random, { detached: true }, function(err){
-      if (err) throw err;
-      if (data && data.name) say.speak('Good job ' + data.name + '!');
-    });
-    
-    process.unref();
-  }
-);
+const channel = pusher.subscribe("palacsinta-channel");
+channel.bind("palacsinta-happened", data => {
+  const random = "sounds/" + (Math.floor(Math.random() * 4) + 1) + ".mp3";
+  const process = player.play(random, { detached: true }, err => {
+    if (err) throw err;
+    if (data && data.name) say.speak("Good job " + data.name + "!");
+  });
 
+  process.unref();
+});
